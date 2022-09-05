8bc898ffb8decf6901b2e75c867b8e35e6e85c54
Piotr <websites@hello-24.com>
2022-09-05 16:01:00 -0500

    Three exercises

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 "use strict";
 
-// Exercise 1
+// Exercise 1 Looping a triangle
 
 for (let line = "#"; line.length < 8; line += "#") console.log(line);
 
-// Exercise 2
+// Exercise 2 FizzBuzz
 
 for (let num = 1; num <= 100; num = num + 1) {
   let output = "";
@@ -20,3 +20,21 @@ for (let num = 1; num <= 100; num = num + 1) {
   }
   console.log(output || num);
 }
+
+// Exercise 3 Chessboard
+
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
